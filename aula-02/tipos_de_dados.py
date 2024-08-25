783176bdd908fc17f8a429bafff99e10ab634fa8
reginaldocoutinho <reginaldo_re12@hotmail.com>
2024-08-24 21:42:15 -0300

    Entendendo como são os tipo de dados no python e formas de interagir com a linguagem de forma interativa pelo prompt de comando

---
+++ aula-02/tipos_de_dados.py
@@ -0,0 +1,13 @@
+print("Inteiro: " + str(1 + 2)) 
+print("Ponto Flutuante: " + str(1.5 + 1 + 0.5))
+valor = True
+print("Boolean: " + str(valor))
+print("String: " + str("Exemplo de Texto"))
+
+# utilizando a função str() consigo concatenar o texto com o resultado da operação
+#Todos os valores passados utilizam de classes como essas abaixo 
+#Para mais informações tem como clicar com o ctrl pressionado em cima das classes para saber mais 
+int()
+
+#No terminal se digitado python consigo fazer os prints acima de forma interativa
+
